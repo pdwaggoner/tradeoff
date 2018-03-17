703462e8174121e54910bc9cf182c03a5b179d05
Philip Waggoner <31326382+pdwaggoner@users.noreply.github.com>
2018-03-16 23:13:16 -0500

    Update and rename measure.R to tradeoff.R

--- tradeoff.R
+++ tradeoff.R
@@ -1,7 +1,10 @@
-# This measure is in a paper currently _Under Review_. Please be sure to let me know if you want to use or work with this. Also, any ideas on making it better are always welcomed!
-# Here is a preliminary, pre-cleaned, rough version of the measure (more categorization and cleaning to come).
-# New to upload data set (here, "dta") with units as items selected by actors (e.g., individual bills sponsored by MCs)
+# This measure is in a paper currently Under Review. Please be sure to let me know if you want to use or work with this.
 
+# Here is a preliminary, pre-cleaned, rough version of the measure (more categorization and cleaning to come).
+
+# Need to upload data set (here, "dta") with units as items selected by actors (e.g., individual bills sponsored by MCs), 
+# Then with "topic" as the category of interest, where 1= the value pursued, and 0= the value not pursued, the standardized difference of which is the tradeoff.
+  
 #Step 1: Total up the number of Priority (P) topics and Non-Priority (NP) topics; this determines the tradeoff being made by the actor (e.g., district vs. non-district; owned vs. trespassed; black vs. red)
 Tot.P.Topics <- sum(dta$Topic==1) # P
 Tot.NP.Topics <- sum(dta$Topic==0) # NP
